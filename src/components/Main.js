92ea4b559920984a454ca962e5db3aed0fb289ff
DeepikaRajapaksha <rpdsewwandi@std.appsc.sab.ac.lk>
2025-04-01 10:12:16 +0530

    Create hourly forecast

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,23 @@
 import "../Styles/components/Main.scss";
 import CurrentWeather from "./CurrentWeather";
+import Forecast from "./Forecast";
+import { getHourlyForecast, getDailyForecast } from "../api";
 function Main() {
   return (
-    <div className='Main'>
+    <div className="Main">
       <CurrentWeather />
+      <Forecast
+        type="hourly"
+        title="Hourly Forecast"
+        data={getHourlyForecast()}
+      />
+      <Forecast
+        type="daily"
+        title="21 Days Forecast"
+        data={getDailyForecast()}
+      />
     </div>
-  )
+  );
 }
 
-export default Main
+export default Main;
